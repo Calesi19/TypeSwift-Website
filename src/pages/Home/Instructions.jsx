88262446eaeb95e53706ddf5af324038afa83dc0
Carlos Lespin <carlos.lespin.silva@gmail.com>
2023-12-21 02:46:40 -0500

    feat: working tailwind container class, instructions almost done, footer complete

--- src/pages/Home/Instructions.jsx
+++ src/pages/Home/Instructions.jsx
@@ -1,10 +1,46 @@
-
 export function Instructions() {
-    return (
-        <div className="bg-[#eb4f26] min-h-[300px]">
+  return (
+    <div className="bg-[#eb4f26] min-h-[300px] w-full py-[150px]">
+      <div className="container w-full">
+        <h1 className="text-5xl">How it Works</h1>
 
-            <h1>How it Works</h1>
+              <div className="min-h-[125px] mt-10 flex gap-8">
+                  
+          <div>
+            <div className="bg-white h-20 w-20 rounded-full text-3xl text-black font-bold flex justify-center items-center">
+              1.
+            </div>
+          </div>
+          <div className="pt-5 text-3xl font-bold">
+            Download and install the<br/>app for your operating<br/>system.
+          </div>
+              </div>
+              
+              <div className="min-h-[125px] mt-8 flex gap-8">
+                  
+                  <div>
+                    <div className="bg-white h-20 w-20 rounded-full text-3xl text-black font-bold flex justify-center items-center">
+                      2.
+                    </div>
+                  </div>
+                  <div className="pt-5 text-3xl font-bold">
+                  Run TypeSwift and<br/>create a new shortcut.
+                  </div>
+              </div>
+              
 
-        </div>
-    )
+              <div className="min-h-[125px] mt-8 flex gap-8">
+                  
+                  <div>
+                    <div className="bg-white h-20 w-20 rounded-full text-3xl text-black font-bold flex justify-center items-center">
+                      3.
+                    </div>
+                  </div>
+                  <div className="pt-5 text-3xl font-bold">
+                  Enjoy typing away. Close<br/>TypeSwift when you’re<br/>finished.
+                  </div>
+                </div>
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}' /* src folder, for example */],
+  content: ["./src/**/*.{html,js,jsx,ts}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
